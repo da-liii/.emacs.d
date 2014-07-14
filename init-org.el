055fdab4dda19d1f9300c60399b46d8484281ee7
Darcy Shen <sadhen1992@gmail.com>
2014-07-14 21:45:55 +0800

    add org mode support

---
+++ init-org.el
@@ -0,0 +1,13 @@
+;; show image
+(require 'iimage)
+(autoload 'iimage-mode "iimage" "Support Inline image minor mode." t)
+(autoload 'turn-on-iimage-mode "iimage" "Turn on Inline image minor mode." t)
+;;org mode show image
+(defun org-toggle-iimage-in-org ()
+  "display images in your org file"
+  (interactive)
+  (if (face-underline-p 'org-link)
+      (set-face-underline-p 'org-link nil)
+      (set-face-underline-p 'org-link t))
+  (iimage-mode))
+(provide 'init-org)
